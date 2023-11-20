f4b75a8371dec36f87e917451bd78054da3e5915
seohwoo <tjguddn9899@naver.com>
2023-11-20 10:58:14 +0900

    basic setting 2023/11/20

---
+++ src/main/java/ex/framework/mvc/ExController.java
@@ -0,0 +1,8 @@
+package ex.framework.mvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ExController {
+
+}
---
+++ src/main/java/test/spring/mvc/SampleController.java
@@ -0,0 +1,8 @@
+package test.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleController {
+
+}
